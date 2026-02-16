0b4918cadc95f4196e483affe018b22fecef5957
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-02-16 16:37:20 +0100

    Updated className of form

--- client/src/app/view/UI/FormContainer.tsx
+++ client/src/app/view/UI/FormContainer.tsx
@@ -3,14 +3,16 @@ import type { ReactNode, SubmitEvent } from 'react';
 type Props = {
   children?: ReactNode;
   onSubmit?: (e: SubmitEvent<HTMLFormElement>) => void;
+  className?: string;
 };
 
-export default function AuthContainer({
+export default function FormContainer({
   children,
   onSubmit,
+  className,
 }: Props) {
   return (
-    <form className='p-2 flex flex-col' onSubmit={onSubmit}>
+    <form className={!className ? `p-2 flex flex-col ${className}` : className} onSubmit={onSubmit}>
       {children}
     </form>
   );
