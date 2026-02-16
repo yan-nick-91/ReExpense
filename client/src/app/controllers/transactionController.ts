b244ede97c70150454883d73894193f421f744a1
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-02-16 14:13:40 +0100

    Init controller

---
+++ client/src/app/controllers/transactionController.ts
@@ -0,0 +1,15 @@
+import { getAllTransaction } from './../api/transactionHttpHandler';
+import { createTransaction } from '../api/transactionHttpHandler';
+import type { AppDispatch } from '../store/store';
+import type { TransactionType } from '../types/transactionTypes';
+
+export const createTransactionController = async (
+  dispatch: AppDispatch,
+  data: { amount: number; category: string; type: TransactionType },
+) => {
+  dispatch(createTransaction(data)).unwrap();
+};
+
+export const getAllTransactionController = async (dispatch: AppDispatch) => {
+  dispatch(getAllTransaction()).unwrap()
+}
