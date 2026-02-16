8ff6b1a9f5247ef1b9a26d5e85e11cc99db76a85
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-02-16 22:18:03 +0100

    Implemented type to open modal

--- client/src/app/view/components/transactions/TransactionComponent.tsx
+++ client/src/app/view/components/transactions/TransactionComponent.tsx
@@ -4,7 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllTransaction } from '../../../api/transactionHttpHandler';
 import type { AppDispatch, RootState } from '../../../store/store';
 
-export default function TransactionComponent() {
+interface TransactionProps {
+  onOpenIncomeModal: () => void
+  onOpenOutcomeModal: () => void
+}
+
+export default function TransactionComponent({ onOpenIncomeModal, onOpenOutcomeModal}: TransactionProps) {
+
   const dispatch = useDispatch<AppDispatch>();
 
   const transactions = useSelector(
@@ -17,17 +23,18 @@ export default function TransactionComponent() {
   }, [dispatch]);
 
   const currentAmount = transactions.reduce((acc, tx) => {
-    if (tx.type === 'income') return acc + tx.amount;
-    if (tx.type === 'outcome') return acc - tx.amount;
+    if (tx.type === 'income') return acc + tx.currency;
+    if (tx.type === 'outcome') return acc - tx.currency;
     return acc;
   }, 0);
 
+
   return (
     <>
-      <div className='mb-4'>Current amount: {currentAmount.toFixed(2)}</div>
+      <p className='mb-4'>Current currency: {currentAmount.toFixed(2)}</p>
       <div className='flex gap-4'>
-        <Button theme={'primary'}>Add</Button>
-        <Button theme={'primary'}>Sub</Button>
+        <Button theme={'primary'} onClick={onOpenIncomeModal}>Income</Button>
+        <Button theme={'primary'} onClick={onOpenOutcomeModal}>Outcome</Button>
       </div>
     </>
   );
