68e5b0527eb99e57ffde66fabc1e8695c3ef35be
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-02-18 10:57:06 +0100

    Code refactoring client types

--- client/src/app/types/authTypes.ts
+++ client/src/app/types/authTypes.ts
@@ -8,8 +8,8 @@ export interface AuthState {
   user?: SafeUser;
   isAuthenticated: boolean;
   error?: string;
-  loading: boolean,
-  success: boolean
+  loading: boolean;
+  success: boolean;
 }
 
 export type SafeUser = Omit<User, 'password'>;
