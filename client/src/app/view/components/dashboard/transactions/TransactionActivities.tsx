3b0695e333ca0bccbaf767180dfbdea902dc7880
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-02-24 21:44:54 +0100

    Modified styling to make the list scrollable

--- client/src/app/view/components/dashboard/transactions/TransactionActivities.tsx
+++ client/src/app/view/components/dashboard/transactions/TransactionActivities.tsx
@@ -15,7 +15,7 @@ export default function TransactionActivities() {
   };
 
   return (
-    <div className='p-4'>
+    <div className='px-4 py-2 mt-3 h-60 overflow-y-scroll'>
       <ul>
         {transactions.map((item) => (
           <li className='mb-2' key={item.id}>
@@ -29,7 +29,13 @@ export default function TransactionActivities() {
               >
                 {item.type === 'expense' ? '-' : '+'} {item.amount.toFixed(2)}
               </p>
-              <Button className='ml-6' theme='primary' navigateTo={`/transactions/${item.id}`}>View transaction</Button>
+              <Button
+                className='ml-6'
+                theme='primary'
+                navigateTo={`/transactions/${item.id}`}
+              >
+                View transaction
+              </Button>
             </div>
             <div className='border border-gray-400' />
           </li>
