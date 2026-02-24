4e20827bdfa5e58ecad0b3638f5437a4f8a153fd
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-02-24 21:46:42 +0100

    Implement GoalsComponent and passing its function

--- client/src/app/view/components/dashboard/DashboardComponent.tsx
+++ client/src/app/view/components/dashboard/DashboardComponent.tsx
@@ -6,26 +6,39 @@ import TransactionActivities from './transactions/TransactionActivities';
 import type { RootState } from '../../../store/store';
 import { useSelector } from 'react-redux';
 import { resetSuccessCreate } from '../../../store/slice/transactionSlice';
+import GoalsComponent from './goals/GoalsComponent';
+import GoalFormModal from './goals/GoalFormModal';
 
 type ExpenseModalType = 'income' | 'expense';
 
 export default function DashboardComponent() {
-  const transactionsSuccess = useSelector((state: RootState) => state.transaction.success)
-  
+  const transactionsSuccess = useSelector(
+    (state: RootState) => state.transaction.success,
+  );
+
   const [activeExpenseModal, setActiveExpenseModal] = useState<
     ExpenseModalType | undefined
   >(undefined);
 
+  const [activeGoalModal, setActiveGoalModal] = useState(false)
+
   useState(() => {
-    document.title = 'ReExpense | Dashboard'
-  })
+    document.title = 'ReExpense | Dashboard';
+  });
 
   const openExpenseModal = (type: ExpenseModalType) => {
     setActiveExpenseModal(type);
-    if (transactionsSuccess) resetSuccessCreate()
+    if (transactionsSuccess) resetSuccessCreate();
   };
 
+  const openGoalModal = () => {
+    setActiveGoalModal(true)
+  }
+
+
   const closeExpenseModal = () => setActiveExpenseModal(undefined);
+
+  const closeGoalModal = () => setActiveGoalModal(false)
 
   return (
     <div className='grid lg:grid-cols-2 w-[90%] m-auto sm:grid-cols-1'>
@@ -47,6 +60,12 @@ export default function DashboardComponent() {
             onOpenOutcomeModal={() => openExpenseModal('expense')}
           />
         </section>
+        <section className='border border-gray-600 rounded-[0.2rem] mb-8'>
+          <div className='bg-[#090979] text-white p-1 pl-2 text-[1.2rem] h-10'>
+            <h2>Goals</h2>
+          </div>
+          <GoalsComponent onOpenGoalModal={openGoalModal}/>
+        </section>
         <section className='border border-gray-600 w-full rounded-[0.2rem]'>
           <div className='bg-[#090979] text-white p-1 pl-2 text-[1.2rem] h-10'>
             <h2>Activities</h2>
@@ -66,6 +85,9 @@ export default function DashboardComponent() {
           expenseType={activeExpenseModal}
           onClose={closeExpenseModal}
         />
+      )}
+      {activeGoalModal && (
+        <GoalFormModal onClose={closeGoalModal}/>
       )}
     </div>
   );
