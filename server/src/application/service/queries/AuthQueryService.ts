5b1ce3421ffa1b87a017a5c09afdcb20bb9347b0
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-02-25 15:23:46 +0100

    Added use case validateResetPasswordToken

--- server/src/application/service/queries/AuthQueryService.ts
+++ server/src/application/service/queries/AuthQueryService.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import { AppDataSource } from '../../../infrastructure/database/data-source.js';
 import { User } from '../../../domain/entities/User.js';
 import type { UserResponseDTO } from '../../dto/out/UserResponseResponse.js';
@@ -11,5 +12,17 @@ export class AuthQueryService {
       where: { id: userId },
     });
     verifyFoundUser(user, 'User not found');
+  }
+
+  async validateResetPasswordToken(token: string) {
+    const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+
+    const user = await this.userRepository.findOne({
+      where: { passwordResetToken: hashedToken },
+    });
+
+    if (!user || !user.passwordResetExpires || user.passwordResetExpires! < new Date()) {
+      throw new Error('Token invalid or expired')
+    }
   }
 }
