987980289384cba54fa8eaa1bf36a75eafab95e0
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-02-25 10:24:31 +0100

    Implemented SkipLink component

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,9 +1,11 @@
-import { Outlet } from 'react-router-dom';
-import NavBar from './view/components/NavBar';
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from './store/store';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import type { AppDispatch } from './store/store';
+
 import { isAuthenticated } from './api/authHttpHandler';
+import NavBar from './view/components/NavBar';
+import SkipLink from './view/UI/SkipLink';
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
@@ -15,6 +17,7 @@ function App() {
   return (
     <>
       <header>
+        <SkipLink />
         <NavBar />
       </header>
       <main>
