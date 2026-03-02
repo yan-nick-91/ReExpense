6d1c82946d6978e35786adb4e0997c7c3d071440
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-03-02 10:28:58 +0100

    Init GoalCommandService

---
+++ server/src/application/service/commands/GoalCommandService.ts
@@ -0,0 +1,62 @@
+import { Goal } from '../../../domain/entities/Goal.js';
+import { NotFoundException } from '../../../domain/exceptions/NotFoundException.js';
+import { AppDataSource } from '../../../infrastructure/database/data-source.js';
+
+import type { GoalDTO } from '../../dto/in/GoalDTO.js';
+import type { GoalResponseDTO } from '../../dto/out/GoalResponseDTO.js';
+import {
+  createToGoalResponseDTO,
+  updateToGoalResponseDTO,
+} from '../../mapper/GoalMapper.js';
+
+export class GoalCommandService {
+  private goalRepository = AppDataSource.getRepository(Goal);
+
+  async create(userId: string, dto: GoalDTO): Promise<GoalResponseDTO> {
+    if (!dto.title) {
+      throw new Error('Title is required');
+    }
+
+    if (dto.savings === 0.0) {
+      throw new Error('Savings should be higher than 0.0');
+    }
+
+    const currentTime = new Date().toISOString();
+    const goal = this.goalRepository.create({
+      user: { id: userId },
+      title: dto.title,
+      description: dto.description,
+      savings: dto.savings,
+      createdAt: currentTime,
+    });
+
+    const savedGoal = await this.goalRepository.save(goal);
+    return createToGoalResponseDTO(savedGoal);
+  }
+
+  async update(
+    userId: string,
+    goalId: string,
+    dto: GoalDTO,
+  ): Promise<GoalResponseDTO> {
+    console.log(userId)
+    const goal = await this.goalRepository.findOne({
+      where: { id: goalId, user: { id: userId } },
+      relations: ['user'],
+    });
+
+    if (!goal) throw new NotFoundException('goal not found');
+
+    const currentTime = new Date().toISOString();
+
+    goal.title = dto.title;
+    goal.description = dto.description;
+    goal.savings = dto.savings;
+    goal.updatedAt = currentTime;
+    console.log(goal.id)
+    console.log(goal.user.id)
+
+    const savedGoal = await this.goalRepository.save(goal);
+    return updateToGoalResponseDTO(savedGoal);
+  }
+}
