d8e8ea0d9641742daa3b63f531d3f70078b65056
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-03-04 10:28:23 +0100

    Updated and adding validation functions:
    - Renamed validationResult to validationStringResult, in order to
    clarify its purpose.
    
    - Added function validationAboveZeroResult

--- client/src/app/validations/globalValidation.ts
+++ client/src/app/validations/globalValidation.ts
@@ -3,10 +3,29 @@ export type ValidationResult = {
   error?: string;
 };
 
-export const validationResult = (
+export const validationStringResult = (
   value: string,
   errorMessage: string,
 ): ValidationResult => {
   if (!value) return { valid: false, error: errorMessage };
+  return { valid: true };
+};
+
+export const validationAboveZeroResult = (
+  value: string,
+  errorMessage: string,
+): ValidationResult => {
+  const trimmed = value.trim();
+
+  if (!trimmed) return { valid: false, error: errorMessage };
+
+  const parsed = +trimmed;
+
+  if (Number.isNaN(parsed)) {
+    return { valid: false, error: 'Must be a valid number' };
+  }
+
+  if (parsed <= 0)
+    return { valid: false, error: 'Must be greater than zero' };
   return { valid: true };
 };
