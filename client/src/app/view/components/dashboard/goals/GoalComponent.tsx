db3073b5784ec6f10614609903c08ce4c1475ab5
yannick coolen <yannick-coolen-1991@hotmail.nl>
2026-03-06 13:43:56 +0100

    Renamed the component

---
+++ client/src/app/view/components/dashboard/goals/GoalComponent.tsx
@@ -0,0 +1,80 @@
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '../../../UI/Button';
+import type { RootState } from '../../../../store/store';
+import { getAllGoalsController } from '../../../../controllers/goalController';
+import { convertToDateAndTimeDisplay } from '../../../../utils/globalFunctions';
+
+type Props = {
+  onOpenGoalModal: () => void;
+};
+
+export default function GoalsComponent({ onOpenGoalModal }: Props) {
+  const dispatch = useDispatch();
+  const goals = useSelector((state: RootState) => state.goal.items);
+
+  useEffect(() => {
+    getAllGoalsController(dispatch);
+  }, [dispatch]);
+
+  const itemDateDisplay = (label: string, dateValue: string) => {
+    return (
+      <div>
+        <span>{label}: </span>
+        <time dateTime={dateValue}>
+          {convertToDateAndTimeDisplay(dateValue)}
+        </time>
+      </div>
+    );
+  };
+
+  const itemDetailDisplay = (label: string, detailString: string) => {
+    return (
+      <div>
+        <span>{label}: </span>
+        <span>{detailString}</span>
+      </div>
+    );
+  };
+
+  return (
+    <div className='flex flex-col'>
+      <div className='flex justify-end p-4'>
+        <Button theme='primary' onClick={onOpenGoalModal}>
+          Add goal
+        </Button>
+      </div>
+      <div className='px-4 py-2 mt-3 h-40 overflow-y-scroll'>
+        {goals.length > 0 && (
+          <ul>
+            {goals.map((goal) => (
+              <li key={goal.id} className='mb-2'>
+                <div className='flex flex-col md:flex-row md:items-center justify-between p-2'>
+                  <div className='mb-2 md:mb-0'>
+                    <h3>Goal: {goal.title}</h3>
+                    {itemDateDisplay('Created at', goal.createdAt)}
+                    {goal.updatedAt &&
+                      itemDateDisplay('Updated at', goal.updatedAt)}
+
+                    {itemDetailDisplay('Price', String(goal.price))}
+                    {itemDetailDisplay(
+                      'Description',
+                      goal.description || 'No Details',
+                    )}
+                  </div>
+                  <div className='flex lg:justify-end'>
+                    <Button navigateTo={`/goals/${goal.id}`} theme='primary'>
+                      View goal
+                    </Button>
+                  </div>
+                </div>
+                <div className='border border-gray-400' />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
