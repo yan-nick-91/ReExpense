57fbe183540dcebdc0b0d15db8f9693249f195a3
yannick coolen <yannick-coolen-1991@outlook.com>
2026-03-09 14:34:21 +0100

    Updated Modal for create and edit purpose

--- client/src/app/view/components/dashboard/goals/GoalFormModal.tsx
+++ client/src/app/view/components/dashboard/goals/GoalFormModal.tsx
@@ -1,16 +1,22 @@
-import clsx from 'clsx';
-import Button from '../../../UI/Button';
 import { useState, type ChangeEvent, type SubmitEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import clsx from 'clsx';
+import {
+  createGoalController,
+  updateGoalController,
+} from '../../../../controllers/goalController';
 import {
   validationAboveZeroResult,
   validationStringResult,
 } from '../../../../validations/globalValidation';
-import { createGoalController } from '../../../../controllers/goalController';
-import { useDispatch } from 'react-redux';
+
+import type { UpdateGoal } from '../../../../types/goalTypes';
+import Button from '../../../UI/Button';
 
 type Props = {
   onClose: () => void;
-  
+  modalPurpose?: ModalPurpose;
+  editGoal?: UpdateGoal;
 };
 
 type FormState = {
@@ -19,13 +25,23 @@ type FormState = {
   price: string;
 };
 
+type UpdateFormState = FormState & {
+  id?: string;
+};
+
 type ErrorState = {
   title?: string;
   description?: string;
   price?: string;
 };
 
-export default function GoalFormModal({ onClose }: Props) {
+type ModalPurpose = 'create' | 'edit';
+
+export default function GoalFormModal({
+  onClose,
+  modalPurpose = 'create',
+  editGoal,
+}: Props) {
   const dispatch = useDispatch();
   const [form, setForm] = useState<FormState>({
     title: '',
@@ -33,24 +49,44 @@ export default function GoalFormModal({ onClose }: Props) {
     price: '',
   });
 
+  const [editForm, setEditForm] = useState<UpdateFormState>(
+    modalPurpose === 'edit' && editGoal
+      ? {
+          id: editGoal.id,
+          title: editGoal.title,
+          description: editGoal.description,
+          price: String(editGoal.price),
+        }
+      : { id: '', title: '', description: '', price: '' },
+  );
+
   const [errors, setErrors] = useState<ErrorState>({});
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
 
-    setForm((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    if (modalPurpose === 'create') {
+      setForm((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    } else {
+      setEditForm((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
   };
 
   const submitGoalHandler = (e: SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    const titleResult = validationStringResult(form.title, 'Title is required');
+    const titleResult = validationStringResult(
+      modalPurpose === 'create' ? form.title : editForm.title,
+      'Title is required',
+    );
 
     const priceResult = validationAboveZeroResult(
-      form.price,
+      modalPurpose === 'create' ? form.price : editForm.price,
       'Price is required',
     );
 
@@ -65,95 +101,110 @@ export default function GoalFormModal({ onClose }: Props) {
 
     if (!isValid) return;
 
-    const priceAsNumber: number = +form.price;
-
+    const priceAsNumber: number =
+      modalPurpose === 'create' ? +form.price : +editForm.price;
     try {
-      createGoalController(dispatch, {
-        title: form.title,
-        description: form.description,
-        price: priceAsNumber,
-      });
+      if (modalPurpose === 'create') {
+        createGoalController(dispatch, {
+          title: form.title,
+          description: form.description,
+          price: priceAsNumber,
+        });
+      } else {
+        updateGoalController(dispatch, {
+          id: editGoal!.id,
+          title: editForm.title,
+          description: editForm.description,
+          price: priceAsNumber,
+        });
+      }
     } catch (err) {
       console.error(err);
     }
   };
-
+  
   return (
-    <div>
-      <section
-        className={clsx(
-          'fixed inset-0 flex items-center justify-center bg-[#1b1b1bbd] bg-opacity-50',
-        )}
+    <section
+      className={clsx(
+        'fixed inset-0 flex items-center justify-center bg-[#1b1b1bbd] bg-opacity-50',
+      )}
+    >
+      <form
+        className='bg-white p-4 rounded-[0.2rem] shadow-lg w-200'
+        onSubmit={submitGoalHandler}
       >
-        <form
-          className='bg-white p-4 rounded-[0.2rem] shadow-lg w-200'
-          onSubmit={submitGoalHandler}
-        >
-          <h2 className='text-xl font-bold mb-4'>Add new goal</h2>
-          <div className='mb-8'>
-            <div className='flex flex-col mb-4'>
-              <label htmlFor='title' className='mb-2'>
-                Title
-              </label>
-              <input
-                id='title'
-                name='title'
-                type='text'
-                value={form.title}
-                onChange={onChange}
-                className='border rounded-[.2rem] mt-1 border-gray-600 p-2'
-                autoComplete='no'
-                aria-invalid={!!errors.title}
-              />
-            </div>
-            {errors.title && (
-              <p className='text-red-600 text-sm' role='alert'>
-                {errors.title}
-              </p>
-            )}
-            <div className='flex flex-col mb-4'>
-              <label htmlFor='description' className='mb-2'>
-                Description
-              </label>
-              <input
-                id='description'
-                name='description'
-                type='description'
-                value={form.description}
-                onChange={onChange}
-                className='border rounded-[.2rem] mt-1 border-gray-600 p-2'
-                autoComplete='no'
-              />
-            </div>
-            <div className='flex flex-col mb-4'>
-              <label htmlFor='price' className='mb-2'>Price</label>
-              <input
-                id='price'
-                name='price'
-                type='number'
-                value={form.price}
-                onChange={onChange}
-                className='border rounded-[.2rem] mt-1 border-gray-600 p-2'
-                autoComplete='no'
-                aria-invalid={!!errors.price}
-              />
-            </div>
-            {errors.price && (
-              <p className='text-red-600 text-sm' role='alert'>
-                {errors.price}
-              </p>
-            )}
+        <h2 className='text-xl font-bold mb-4'>
+          {modalPurpose === 'create' ? 'Add new goal' : 'Update goal'}
+        </h2>
+        <div className='mb-8'>
+          <div className='flex flex-col mb-4'>
+            <label htmlFor='title' className='mb-2'>
+              Title
+            </label>
+            <input
+              id='title'
+              name='title'
+              type='text'
+              value={modalPurpose === 'create' ? form.title : editForm.title}
+              onChange={onChange}
+              className='border rounded-[.2rem] mt-1 border-gray-600 p-2'
+              autoComplete='no'
+              aria-invalid={!!errors.title}
+            />
           </div>
-          <div className='flex gap-4'>
-            <Button theme='primary' type='submit'>
-              Submit Transaction
-            </Button>
-            <Button theme='primary' onClick={onClose}>
-              Close Form
-            </Button>
+          {errors.title && (
+            <p className='text-red-600 text-sm' role='alert'>
+              {errors.title}
+            </p>
+          )}
+          <div className='flex flex-col mb-4'>
+            <label htmlFor='description' className='mb-2'>
+              Description
+            </label>
+            <input
+              id='description'
+              name='description'
+              type='description'
+              value={
+                modalPurpose === 'create'
+                  ? form.description
+                  : editForm.description
+              }
+              onChange={onChange}
+              className='border rounded-[.2rem] mt-1 border-gray-600 p-2'
+              autoComplete='no'
+            />
           </div>
-        </form>
-      </section>
-    </div>
+          <div className='flex flex-col mb-4'>
+            <label htmlFor='price' className='mb-2'>
+              Price
+            </label>
+            <input
+              id='price'
+              name='price'
+              type='number'
+              value={modalPurpose === 'create' ? form.price : editForm.price}
+              onChange={onChange}
+              className='border rounded-[.2rem] mt-1 border-gray-600 p-2'
+              autoComplete='no'
+              aria-invalid={!!errors.price}
+            />
+          </div>
+          {errors.price && (
+            <p className='text-red-600 text-sm' role='alert'>
+              {errors.price}
+            </p>
+          )}
+        </div>
+        <div className='flex gap-4'>
+          <Button theme='primary' type='submit'>
+            Submit Transaction
+          </Button>
+          <Button theme='primary' onClick={onClose}>
+            Close Form
+          </Button>
+        </div>
+      </form>
+    </section>
   );
 }
