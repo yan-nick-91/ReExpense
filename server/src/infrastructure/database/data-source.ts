ec7d4c7bd4483d46710a62500f455c99b541509b
yannick coolen <yannick-coolen-1991@outlook.com>
2026-03-13 17:59:22 +0100

    Added Saving as entity for data source

--- server/src/infrastructure/database/data-source.ts
+++ server/src/infrastructure/database/data-source.ts
@@ -1,12 +1,13 @@
 import { DataSource } from 'typeorm';
-import { User } from '../../domain/entities/User.js';
-import { Transaction } from '../../domain/entities/Transaction.js';
+import { Saving } from '../../domain/entities/Saving.js';
 import { Goal } from '../../domain/entities/Goal.js';
+import { Transaction } from '../../domain/entities/Transaction.js';
+import { User } from '../../domain/entities/User.js';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: 'reexpense.sqlite',
   synchronize: true,
   logging: false,
-  entities: [Goal, Transaction, User],
+  entities: [Goal, Saving, Transaction, User],
 });
