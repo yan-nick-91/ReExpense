25ef6f7eeba9e974c328963dc40fb329ef4bab8d
yannick coolen <yannick-coolen-1991@outlook.com>
2026-03-13 17:57:37 +0100

    Init SavingQueryService

---
+++ server/src/application/service/queries/SavingQueryService.ts
@@ -0,0 +1,34 @@
+import { AppDataSource } from '../../../infrastructure/database/data-source.js';
+import { Saving } from '../../../domain/entities/Saving.js';
+import type { SavingResponseDTO } from '../../dto/out/SavingResponseDTO.js';
+import { NotFoundException } from '../../../domain/exceptions/NotFoundException.js';
+
+export class SavingQueryService {
+  private getRepository = AppDataSource.getRepository(Saving);
+
+  async getAllSavingBalances(userId: string): Promise<SavingResponseDTO[]> {
+    const savings = await this.getRepository
+      .createQueryBuilder('saving')
+      .leftJoinAndSelect('saving.user', 'user')
+      .where('user.id = :userId', { userId })
+      .getMany();
+
+    return savings.map((saving) => {
+      return { id: saving.id, name: saving.name, balance: saving.balance };
+    });
+  }
+
+  //   async getSavingBySavingId(savingId: string): Promise<SavingResponseDTO> {
+  //     const saving = await this.getRepository.findOne({
+  //       where: { id: savingId },
+  //     });
+
+  //     if (!saving) throw new NotFoundException('Saving not found');
+
+  //     return {
+  //       id: saving.id,
+  //       name: saving.name,
+  //       balance: saving.balance,
+  //     };
+  //   }
+}
