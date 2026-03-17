f0d95871112bea5a0a3b07cf6fda330429fe8a62
yannick coolen <yannick-coolen-1991@outlook.com>
2026-03-17 23:34:55 +0100

    Implemented custom exceptions and refactoring code

--- server/src/domain/business/validations.ts
+++ server/src/domain/business/validations.ts
@@ -1,24 +1,30 @@
 import type { User } from '../entities/User.js';
-import { InvalidCredentialsException } from '../exceptions/InvalidCredentialsException.js';
-import { isTokenRevoked } from '../../infrastructure/token.store.js';
+import {
+  EmailAlreadyUsedException,
+  InvalidCredentialsException,
+  InvalidEmailException,
+  InvalidPasswordLengthException,
+} from '../exceptions/AuthenticationExceptions.js';
 
 export const isValidEmail = (email: string) => {
   const EMAIL_REGEX = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-
   if (!EMAIL_REGEX.test(email.trim())) {
-    throw new Error('Invalid email format');
+    throw new InvalidEmailException('Invalid email format');
   }
 };
 
 export const verifyPasswordLength = (password: string) => {
-  if (password.length < 8) {
-    throw new Error('Password must at least 8 characters');
+  const MINIMUM_PASSWORD_LENGTH = 8;
+  if (password.length < MINIMUM_PASSWORD_LENGTH) {
+    throw new InvalidPasswordLengthException(
+      'Password must at least 8 characters',
+    );
   }
 };
 
 export const emailAlreadyTaken = (existingUser: User | null) => {
   if (existingUser) {
-    throw new Error('Email already in use');
+    throw new EmailAlreadyUsedException('Email already in use');
   }
 };
 
@@ -28,10 +34,4 @@ export const verifyFoundUser = (user: User | null, errorMessage: string) => {
 
 export const verifyPassword = (isValid: boolean) => {
   if (!isValid) throw new InvalidCredentialsException('Invalid credentials');
-};
-
-export const verifyIfTokenIsRevoked = (token: string) => {
-  if (isTokenRevoked(token)) {
-    throw new Error('');
-  }
 };
