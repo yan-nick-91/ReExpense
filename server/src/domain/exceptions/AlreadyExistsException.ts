496de5db5d3c53691c789b59ba1031973e9e8a78
yannick coolen <yannick-coolen-1991@outlook.com>
2026-03-17 23:29:57 +0100

    Removed custom exception

--- server/src/domain/exceptions/AlreadyExistsException.ts
+++
@@ -1,7 +0,0 @@
-import AppException from './AppException.js';
-
-export class AlreadyExistsException extends AppException {
-  constructor(message: string) {
-    super(message);
-  }
-}
--- server/src/domain/exceptions/InvalidCredentialsException.ts
+++
@@ -1,7 +0,0 @@
-import AppException from './AppException.js';
-
-export class InvalidCredentialsException extends AppException {
-  constructor(message: string) {
-    super(message);
-  }
-}
