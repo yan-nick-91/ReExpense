b8601df000ba74904937fdf62513c546475d857e
yannick coolen <yannick-coolen-1991@outlook.com>
2026-03-19 11:46:45 +0100

    Removed generators.ts

--- server/src/application/utils/generators.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto'
-
-export const generateResetToken = () => {
-    const resetToken = crypto.randomBytes(32).toString('hex')
-    const hashedToken = crypto.createHash('sha256').update(resetToken).digest('hex')
-    return { resetToken, hashedToken }
-}
