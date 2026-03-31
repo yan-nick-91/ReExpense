57bc94039960d32ada1821ffc82215ec6a03919a
yannick coolen <yannick-coolen-1991@outlook.com>
2026-03-31 17:37:46 +0200

    Deleted file InsufficientAmountException

--- server/src/domain/exceptions/InsufficientAmountException.ts
+++
@@ -1,7 +0,0 @@
-import AppException from "./AppException.js";
-
-export class InsufficientAmountException extends AppException {
-    constructor(message: string) {
-        super(message)
-    }
-}
